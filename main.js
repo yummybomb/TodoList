fe5f84e279661a4782592a8405b5245b61d72b77
yummybomb <nickchen12345@gmail.com>
2022-10-30 03:02:12 -0400

    CSS finishing touches

--- main.js
+++ main.js
@@ -137,10 +137,13 @@ function getTodos() {
     } else {
       todos = JSON.parse(localStorage.getItem("todos"));
     }
+    //reverse array
     for (let i = todos.length-1; i >=0 ; i--){
         temp.push(todos[i]);
     }
     todos = temp;
+    //save reversed array into storage
     localStorage.setItem("todos", JSON.stringify(todos));
+    //reload page
     location.reload();
   }
